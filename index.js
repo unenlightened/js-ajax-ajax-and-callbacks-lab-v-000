e67aa63daf079a883597c71fa24ab53e99469dfc
Chely Ho <adept_inept@yahoo.com>
2018-07-11 19:13:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
 });
 
 function searchRepositories(){
-  let term = $("#searchTerms").val
+  let term = $("#searchTerms").val()
   let url = `https:/api.github.com/search/repositories/?=${term}`
   $.get(url, function(response) {
     $("#results").html(response)
